9beec97ce9629ac32c089bb6816fff510706b377
Donghyeok Lim <zx07050@naver.com>
2021-08-27 19:08:22 +0900

    [BOJ] 2798 블랙잭

---
+++ 2798/baekjoon2798.py
@@ -0,0 +1,30 @@
+n, m = map(int,input().split())
+card = list(map(int,input().split()))
+
+card_sum = []
+
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if card[i] + card[j] + card[k] <= m:
+                card_sum.append(card[i] + card[j] + card[k])
+
+print(max(card_sum))
+
+
+'''
+n, m = map(int,input().split())
+card = list(map(int,input().split()))
+
+card_sum = 0
+
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if card[i] + card[j] + card[k] > m:
+                continue
+            else:
+                card_sum = max(card_sum, card[i] + card[j] + card[k])
+
+print(card_sum)
+'''
