f2851dffe3cac4ce27407d09337519e217944017
Donghyeok Lim <zx07050@naver.com>
2021-08-28 14:07:02 +0900

    [BOJ] 2231 분해합

---
+++ 2231/baekjoon2231.py
@@ -0,0 +1,13 @@
+n = int(input())
+result = 0
+
+for i in range(1, n+1):
+    arr = list(map(int,str(i)))
+    result = i + sum(arr)
+
+    if result == n:
+        print(i)
+        break
+    
+    if i == n:
+        print(0)
