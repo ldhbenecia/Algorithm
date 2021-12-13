1c744dba79538a91cc4db9cf31784fd7f7c1404d
DongHyeok Lim <zx07050@naver.com>
2021-12-13 16:55:05 +0900

    [BOJ] 10866 덱

---
+++ BOJ/10866/baekjoon10866.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+
+deque = deque()
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    
+    if command[0] == 'push_front':
+        deque.appendleft(command[1])
+        
+    if command[0] == 'push_back':
+        deque.append(command[1])
+
+    elif command[0] == 'pop_front':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.popleft())
+            
+    elif command[0] == 'pop_back':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque.pop())
+
+    elif command[0] == 'size':
+        print(len(deque))
+
+    elif command[0] == 'empty':
+        if len(deque) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif command[0] == 'front':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[0])
+            
+    elif command[0] == 'back':
+        if len(deque) == 0:
+            print(-1)
+        else:
+            print(deque[-1])
