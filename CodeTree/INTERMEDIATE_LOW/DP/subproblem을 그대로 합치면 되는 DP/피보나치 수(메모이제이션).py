7fd0f0cba035257d3e50f50ab66729550918708b
DongHyeok Lim <zx07050@naver.com>
2022-08-12 10:46:10 +0900

    [CodeTree] 피보나치 수

---
+++ CodeTree/INTERMEDIATE_LOW/DP/subproblem을 그대로 합치면 되는 DP/피보나치 수(메모이제이션).py
@@ -0,0 +1,18 @@
+# 메모이제이션
+n = int(input())
+
+UNUSED = -1
+
+memo = [UNUSED for _ in range(n + 1)]
+
+def fib(n):
+    if memo[n] != UNUSED:
+        return memo[n]
+    
+    if n == 1 or n == 2:
+        return 1
+    
+    memo[n] = fib(n - 1) + fib(n - 2)
+    return memo[n]
+
+print(fib(n))
