714782c83bec64fa2fd7416d199a29400bafee31
DongHyeok Lim <zx07050@naver.com>
2022-12-14 21:28:44 +0900

    Stair jump - DP 수정

--- Hufs_Algorithm/Dynamic_Programming/jump.py
+++ Hufs_Algorithm/Dynamic_Programming/jump.py
@@ -1,14 +1,13 @@
 def jump(n): # n층에 도달하는 경우의 수를 return
   A = [0] * (n+1)
-  answer = 0
   
   A[1] = 1
-  A[2] = 2
+  A[2] = 1
+  A[3] = 2
   
   for i in range(3, n+1):
     A[i] = A[i-1] + A[i-2]
     
-  answer = A[n]
-  return answer
+  return A[n]
 
 print(jump(7))
