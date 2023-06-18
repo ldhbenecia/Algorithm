2a72198303d2f069039bbb09f287a3ecb7424ce8
DongHyeok Lim <jh07050@gmail.com>
2023-06-18 17:32:32 +0900

    [이코테] 상하좌우

--- 이코테/구현/상하좌우.py
+++ 이코테/구현/상하좌우.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 move = input().split()
 
@@ -17,3 +18,40 @@ for i in move:
     x, y = nx, ny
     
 print(x, y)
+'''
+
+n = int(input())
+command = input().split()
+
+x, y = 1, 1 # 초기 좌표
+
+# 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for i in command:
+    if i == "L":
+        nx = x + dx[2]
+        ny = y + dy[2]
+        
+    if i == "R":
+        nx = x + dx[3]
+        ny = y + dy[3]
+        
+    if i == "U":
+        nx = x + dx[0]
+        ny = y + dy[0]
+        
+    if i == "D":
+        nx = x + dx[1]
+        ny = y + dy[1]
+        
+    # 맵을 뚫었을 때
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue # 무시하고 진행
+    
+    x, y = nx, ny
+    
+print(x, y)
+
+
