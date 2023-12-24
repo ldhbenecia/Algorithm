cfefc9d5128d168bd4e6df095909d31d28242acf
Donghyeok Lim <jh07050@gmail.com>
2023-12-24 18:15:59 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 568 ms, Memory: 251984 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,19 +1,25 @@
-n = int(input())
-num = list(map(int, input().split()))
+# O(n)
+# -10,000,000 < N < 10,000,000
 
+n = int(input())
+num = map(int, input().split())
 m = int(input())
-find_num = list(map(int, input().split()))
+num_count = map(int, input().split())
 
 dic = {}
+result = []
 
 for i in num:
-  if i not in dic:
-    dic[i] = 1
-  else:
-    dic[i] += 1
-
-for i in find_num:
   if i in dic:
-    print(dic[i], end = " ")
+    dic[i] += 1
   else:
-    print(0, end = " ")
+    dic[i] = 1
+    
+for i in num_count:
+  if i in dic:
+    result.append(dic[i])
+  else:
+    result.append(0)
+
+for i in result:
+  print(i, end=" ")
