fd9383a88805f0dea19c0beb3aaa5895da9225cf
Donghyeok Lim <jh07050@gmail.com>
2023-12-24 18:53:00 +0900

    [Silver II] Title: 쇠막대기, Time: 128 ms, Memory: 117128 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,17 @@
+bar = input()
+
+stack = []
+count = 0
+
+for i in range(len(bar)):
+    if bar[i] == '(':
+        stack.append(bar[i])
+    else: # ')' 가 나왔을 때
+        if bar[i-1] == '(': # '()'가 된다면
+            stack.pop() # 막대기가 아니라 레이저이므로 
+            count += len(stack)
+        else:
+            stack.pop()
+            count += 1
+            
+print(count)
