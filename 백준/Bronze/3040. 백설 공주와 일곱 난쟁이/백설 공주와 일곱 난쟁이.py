d3229cf9f899ffcd1965c861eb795f608e91eb87
Donghyeok Lim <jh07050@gmail.com>
2023-12-28 19:35:02 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -1,11 +1,9 @@
+from itertools import combinations
 n = [int(input()) for _ in range(9)]
 
-for i in range(8):
-  for j in range(i + 1, 9):
-    if sum(n) - n[i] - n[j] == 100:
-      n[i], n[j] = 0, 0
-
-result = [i for i in n if i != 0]
+result = list(combinations(n, 7))
 
 for i in result:
-  print(i)
+  if sum(i) == 100:
+    for j in i:
+      print(j)
