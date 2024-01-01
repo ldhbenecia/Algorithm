a1b5474a138205a05a6bc33c194a67d4ffd0feb5
Donghyeok Lim <jh07050@gmail.com>
2024-01-01 18:03:19 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 220 ms, Memory: 112684 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -0,0 +1,23 @@
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  tree = list(map(int, input().split()))
+  tree.sort()
+  
+  left = []
+  right = []
+  for i in range(0, n, 2):
+    left.append(tree[i])
+  for i in range(1, n, 2):
+    right.append(tree[i])
+  right.sort(reverse=True)
+  
+  trees = left + right
+  
+  result = 0
+  for i in range(1, n):
+    result = max(result, abs(trees[i] - trees[i-1]))
+  
+  print(result)
+    
