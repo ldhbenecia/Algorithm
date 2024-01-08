245a5d092da27b6fb0a564c0dbb413b2df7714fb
Donghyeok Lim <jh07050@gmail.com>
2024-01-08 16:16:38 +0900

    [Silver I] Title: 쿼드트리, Time: 168 ms, Memory: 112580 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,26 +1,24 @@
-# 1 <= N <= 64
-# 거의 모든 알고리즘 가능, 브루트포스, 완탐 가능
-
 n = int(input())
 graph = [list(map(int, input())) for _ in range(n)]
 
-def dfs(x, y, n):
+def backtracking(r, c, n):
   is_same = True
-  for i in range(x, x + n):
-    for j in range(y, y + n):
-      if graph[x][y] != graph[i][j]:
+
+  for i in range(r, r + n):
+    for j in range(c, c + n):
+      if graph[r][c] != graph[i][j]:
         is_same = False
         break
-      
-  if is_same:
-    print(graph[x][y], end="")
-  else:
-    print("(", end="")
-    n = n // 2
-    dfs(x, y, n)
-    dfs(x, y + n, n)
-    dfs(x + n, y, n)
-    dfs(x + n, y + n, n)
-    print(")", end="")
 
-dfs(0, 0, n)
+  if is_same:
+    print(graph[r][c], end="")
+  else:
+    half_size = n // 2
+    print("(", end="")
+    backtracking(r, c, half_size)
+    backtracking(r, c + half_size, half_size)
+    backtracking(r + half_size, c, half_size)
+    backtracking(r + half_size, c + half_size, half_size)
+    print(")", end="")
+  
+backtracking(0, 0, n)
