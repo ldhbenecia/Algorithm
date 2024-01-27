f4864390fe1a27c6aeff7416df908b9ecf46adee
Donghyeok Lim <jh07050@gmail.com>
2024-01-27 17:16:06 +0900

    [Silver III] Title: 어린 왕자, Time: 124 ms, Memory: 110272 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -10,10 +10,12 @@ for _ in range(t):
   for _ in range(n):
     cx, cy, r = map(int, input().split())
     
-    distance_start = (x1 - cx)**2 + (y1 - cy)**2
-    distance_end = (x2 - cx)**2 + (y2 - cy)**2
+    is_start_point_inner_circle = ((x1 - cx)**2 + (y1 - cy)**2) <= r**2
+    is_end_point_inner_circle = ((x2 - cx)**2 + (y2 - cy)**2) <= r**2
     
-    if (distance_start < r**2 and distance_end > r**2) or (distance_start > r**2 and distance_end < r**2):
+    if is_start_point_inner_circle is True and is_end_point_inner_circle is False:
+      count += 1
+    elif is_start_point_inner_circle is False and is_end_point_inner_circle is True:
       count += 1
 
   print(count)
