ae053b7461626dbd3c5ca36fbb385ededc0e2407
Donghyeok Lim <jh07050@gmail.com>
2024-02-07 18:36:20 +0900

    [Silver II] Title: 색종이 만들기, Time: 144 ms, Memory: 110996 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,26 +1,30 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 white, blue = 0, 0
 
-def dfs(x, y, n):
-  global white, blue;
-  half_size = n // 2
+def solution(n, x, y):
+  global white, blue
+  
+  half = n // 2
   color = graph[x][y]
   
   for i in range(x, x + n):
     for j in range(y, y + n):
-      if color != graph[i][j]:
-        dfs(x, y, half_size) # 2사분면
-        dfs(x, y + half_size, half_size) # 1사분면
-        dfs(x + half_size, y, half_size) # 3사분면
-        dfs(x + half_size, y + half_size, half_size) # 4사분면
+      if graph[i][j] != color:
+        solution(half, x, y)
+        solution(half, x + half, y)
+        solution(half, x, y + half)
+        solution(half, x + half, y + half)
         return
-  
+        
   if color == 0:
     white += 1
   else:
     blue += 1
-  
-dfs(0, 0, n)
+      
+solution(n, 0, 0)
 print(white)
 print(blue)
