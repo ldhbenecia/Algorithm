40f278dbd04cab9e0522fee99a12212a5665a9b8
Donghyeok Lim <jh07050@gmail.com>
2024-02-20 17:02:02 +0900

    [level 1] Title: 덧칠하기, Time: 1.56 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,10 @@
+def solution(n, m, section):
+    answer = 0
+    paint = 0
+    
+    for i in section:
+        if i > paint:
+            paint = i + m - 1
+            answer += 1
+    
+    return answer
