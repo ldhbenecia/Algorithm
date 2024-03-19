e922ffc8e6ff9b1d14a73238e27b5d1c3013d245
Donghyeok Lim <jh07050@gmail.com>
2024-03-19 16:55:35 +0900

    [level 2] Title: 전화번호 목록, Time: 86.56 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,9 +2,8 @@ def solution(phone_book):
     answer = True
     phone_book.sort()
     
-    for i in range(len(phone_book) - 1):
-        slice_value = len(phone_book[i])
-        if phone_book[i] == phone_book[i+1][:slice_value]:
-            answer = False
+    for i, j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
+            return False
             
     return answer
