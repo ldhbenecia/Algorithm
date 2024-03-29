aa8e4d2f87fdce4611787d96c66758f97fa280a7
Donghyeok Lim <jh07050@gmail.com>
2024-03-29 20:24:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,27 +1,23 @@
+import heapq
+
 def solution(operations):
     answer = []
-    result = []
     
     for operate in operations:
         command, value = operate.split()
         value = int(value)
         
         if command == "I":
-            answer.append(value)
+            heapq.heappush(answer, value)
         if command == "D":
             if answer and value == 1:
-                max_value = max(answer)
-                answer.remove(max_value)
+                answer.pop()
             if answer and value == -1:
-                min_value = min(answer)
-                answer.remove(min_value)
+                heapq.heappop(answer)
     
     if not answer:
-        result = [0, 0]
+        answer = [0, 0]
     else:
-        max_value = max(answer)
-        min_value = min(answer)
-        result.append(max_value)
-        result.append(min_value)
+        answer = [max(answer), min(answer)]
         
-    return result
+    return answer
