e3f6c925e17204ff8d05c449b71bd485d5f9f100
Donghyeok Lim <jh07050@gmail.com>
2024-03-30 15:10:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,9 @@
 def solution(array, commands):
     answer = []
-    result = []
     
     for command in commands:
         i, j, k = command[0], command[1], command[2]
-        answer = sorted(array[i-1:j])
-        result.append(answer[k-1])
+        section = sorted(array[i-1:j])
+        answer.append(section[k-1])
 
-    return result
+    return answer
