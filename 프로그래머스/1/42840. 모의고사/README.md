a63470c7cb89894b1e0d5ccbcced8d2a9c86d619
Donghyeok Lim <jh07050@gmail.com>
2024-04-03 15:49:43 +0900

    [level 1] Title: 모의고사, Time: 2.54 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 4.57 ms
+메모리: 10.6 MB, 시간: 2.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 14:58:17
+2024년 04월 03일 15:49:41
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,23 +3,30 @@ def solution(answers):
     a1 = [1, 2, 3, 4, 5]
     a2 = [2, 1, 2, 3, 2, 4, 2, 5]
     a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    c1, c2, c3 = 0, 0, 0
-    
+    correct = {"1": 0, "2": 0, "3": 0}
+
     for i in range(len(answers)):
         if answers[i] == a1[i % 5]:
-            c1 += 1
+            correct["1"] += 1
         if answers[i] == a2[i % 8]:
-            c2 += 1
+            correct["2"] += 1
         if answers[i] == a3[i % 10]:
-            c3 += 1
-            
-    high = max(c1, c2, c3)
+            correct["3"] += 1
     
-    if high == c1:
-        answer.append(1)
-    if high == c2:
-        answer.append(2)
-    if high == c3:
-        answer.append(3)
-
+    max_value = 0
+    for _, value in correct.items():
+        if max_value < value:
+            max_value = value
+    
+    max_list = []
+    for key, value in correct.items():
+        if max_value == value:
+            max_list.append(key)
+    
+    if len(max_list) == 1:
+        return list(map(int, max_list))
+    else:
+        max_list.sort()
+        return list(map(int, max_list))
+        
     return answer
