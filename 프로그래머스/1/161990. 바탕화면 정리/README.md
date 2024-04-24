7c95b51184a7b8c1c2efe042b1bbb6537466d752
Donghyeok Lim <jh07050@gmail.com>
2024-04-24 16:33:29 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=python3) 
 
 ### 성능 요약
 
-메모리: 71.2 MB, 시간: 0.17 ms
+메모리: 10 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 16:31:23
+2024년 04월 24일 16:33:27
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,18 +1,15 @@
 def solution(wallpaper):
     answer = []
     
-    lux, luy = 0, 0
+    lux, luy = 50, 50
     rdx, rdy = 0, 0
     
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == '#':
-                answer.append((i, j))
+                lux = min(i, lux)
+                luy = min(j, luy)
+                rdx = max(i, rdx)
+                rdy = max(j, rdy)
     
-    lux = min(answer)[0]
-    luy = min(answer, key=lambda x: x[1])[1]
-    rdx = max(answer)[0] + 1
-    rdy = max(answer, key=lambda x: x[1])[1] + 1
-    
-    
-    return [lux, luy, rdx, rdy]
+    return [lux, luy, rdx + 1, rdy + 1]
