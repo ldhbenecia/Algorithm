51ccad985fd9750996c40d998e864486b265c84c
Donghyeok Lim <jh07050@gmail.com>
2024-04-25 18:14:38 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -2,11 +2,15 @@ def solution(N, stages):
     dic = {}
     answer = []
     challenger_cnt = len(stages)
+    fail_count_list = [0]
+    
+    for i in range(1, N + 1):
+        fail_count_list.append(stages.count(i))
     
     for i in range(1, N + 1):
         if challenger_cnt != 0:
-            dic[i] = (stages.count(i) / (challenger_cnt))
-            challenger_cnt -= stages.count(i)
+            dic[i] = (fail_count_list[i] / (challenger_cnt))
+            challenger_cnt -= fail_count_list[i]
         else:
             dic[i] = 0
             
