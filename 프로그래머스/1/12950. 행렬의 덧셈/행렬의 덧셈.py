b2cde35dcfe9efd83462f07b6489c7520ddc250b
Donghyeok Lim <jh07050@gmail.com>
2024-05-01 14:07:54 +0900

    [level 1] Title: 행렬의 덧셈, Time: 38.55 ms, Memory: 22.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    answer = []
+
+    for i in range(len(arr1)):
+        save = []
+        for j in range(len(arr1[0])):
+            save.append(arr1[i][j] + arr2[i][j])
+        answer.append(save)
+    return answer
