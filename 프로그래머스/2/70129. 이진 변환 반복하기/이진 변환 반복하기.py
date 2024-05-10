fa1544c1bee06b03b09134cb4373388da12f4278
Donghyeok Lim <jh07050@gmail.com>
2024-05-10 21:32:20 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.49 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    count = 0
+    zero_count = 0
+    
+    while True:
+        if s == '1':
+            break
+        zero_count += s.count('0')
+        s = s.replace('0', '')
+        s = bin(len(s))[2:] 
+        
+        count += 1
+    return [count, zero_count]
