da539f04856098a71ae88a1f8a035e303e8509d1
Donghyeok Lim <jh07050@gmail.com>
2024-05-13 15:57:25 +0900

    [Gold V] Title: 토마토, Time: 420 ms, Memory: 156640 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,47 +1,38 @@
 from collections import deque
 
+def bfs():
+  while queue:
+    x, y = queue.popleft()
+    
+    for i in range(4):
+      nx = dx[i] + x
+      ny = dy[i] + y
+      
+      if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+        graph[nx][ny] = graph[x][y] + 1
+        queue.append((nx, ny))
+        
+  for i in range(n):
+    for j in range(m):
+      if graph[i][j] == 0:
+        return -1
+      
+  max_day = max(max(row) for row in graph)
+  return max_day - 1
+  
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-queue = deque()
-
-# 상 하 좌 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = 0
+queue = deque([])
 
-# 익은 토마토 위치 삽입
 for i in range(n):
   for j in range(m):
     if graph[i][j] == 1:
-      queue.append([i, j])
+      queue.append((i, j))
 
-# 최소 일수를 구하는 것 -> BFS로 풀이
-def bfs():
-  while queue:
-    x, y = queue.popleft() # 익은 토마토 위치
-    
-    # 상하좌우 탐색
-    for i in range(4):
-      nx, ny = dx[i] + x, dy[i] + y
-      # 그래프 범위 안이고 익지 않은 경우 (0)
-      if 0 <= nx < n and 0 <= ny < m :
-        if graph[nx][ny] == 0:
-          graph[nx][ny] = graph[x][y] + 1 # 익힘 전염 +1 
-          queue.append([nx, ny])
-  
-bfs()
-
-flag = True
-for i in graph:
-  for j in i:
-    if j == 0:
-      flag = False
-    
-  result = max(result, max(i))
-
-if flag == False:
-  print(-1)
-else:
-  print(result - 1)
+        
+# 모든 토마토가 익었을 때의 일수 출력
+print(bfs())
