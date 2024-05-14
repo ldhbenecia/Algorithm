277fa0120a36eb6cf386d31cff487873721e3ef4
Donghyeok Lim <jh07050@gmail.com>
2024-05-14 17:25:34 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 200 ms, Memory: 133028 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+
+def bfs(a, b):
+  queue = deque([(a, 0)])
+  shortest_time = 100000
+  count = 0
+
+  while queue:
+    position, time = queue.popleft()
+    visited[position] = 1
+
+    if position == b and time <= shortest_time:
+      shortest_time = min(shortest_time, time)
+      count += 1
+    if time > shortest_time:
+      break
+    
+    for i in [position - 1, position + 1, position * 2]:
+      if 0 <= i <= 100000 and not visited[i]:
+        queue.append((i, time + 1))
+
+  return shortest_time, count
+
+n, k = map(int, input().split())
+visited = [0] * 100001
+
+shortest_time, num_ways = bfs(n, k)
+print(shortest_time)
+print(num_ways)
