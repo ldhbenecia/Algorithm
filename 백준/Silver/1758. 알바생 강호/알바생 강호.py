ab3384702cef1b5ae183b7d94db05b46fc66d3c6
Donghyeok Lim <jh07050@gmail.com>
2024-05-16 17:10:39 +0900

    [Silver IV] Title: 알바생 강호, Time: 164 ms, Memory: 111612 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -0,0 +1,14 @@
+n = int(input())
+tips = [int(input()) for _ in range(n)]
+tips.sort(reverse=True)
+
+answer = 0
+for i in range(1, n + 1):
+  tip = tips[i - 1] - (i - 1)
+  
+  if tip < 0:
+    continue
+  else:
+    answer += tip
+  
+print(answer)
