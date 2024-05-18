bbbac3c1b30ee4458e5886bde9f99020726fc6ee
Donghyeok Lim <jh07050@gmail.com>
2024-05-18 22:06:23 +0900

    [Silver I] Title: 스타트링크, Time: 208 ms, Memory: 127020 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -2,21 +2,21 @@ from collections import deque
 
 def bfs(start):
   queue = deque([(start)])
-  visited[start] = 1
+  visited[start] = 0 # 시작 층은 횟수 0으로
   
   while queue:
     v = queue.popleft()
     
     if v == G:
-      return visited[v] - 1
+      return visited[v]
     
     for i in (v + U, v - D):
-      if 0 < i <= F and visited[i] == 0:
+      if 0 < i <= F and visited[i] == -1:
         visited[i] = visited[v] + 1
         queue.append(i)
         
   return 'use the stairs'
 
 F, S, G, U, D = map(int, input().split())
-visited = [0 for _ in range(F + 1)]
+visited = [-1 for _ in range(F + 1)]
 print(bfs(S))
