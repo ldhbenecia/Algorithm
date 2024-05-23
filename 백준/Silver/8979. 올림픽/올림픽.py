399a0c3a3649b156f0ca41894319de3968d8cac3
Donghyeok Lim <jh07050@gmail.com>
2024-05-23 22:00:38 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 109240 KB, Score: 65 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+medal_list = [list(map(int, input().split())) for _ in range(n)]
+medal_list.sort()
+
+k, kg, ks, kb = medal_list[k-1]
+rank = 1
+
+for nation, gold, silver, bronze in medal_list:
+  if k == nation:
+    continue
+  if gold > kg or (gold == kg and silver > ks) or (gold == kg and silver == ks and bronze > kg):
+    rank += 1
+    
+print(rank)
+  
