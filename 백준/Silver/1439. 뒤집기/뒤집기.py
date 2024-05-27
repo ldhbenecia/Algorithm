8621c3af6de4ee487a99cbffa65c7431dee7ab01
DongHyeok Lim <jh07050@gmail.com>
2024-05-27 18:35:54 +0900

    [Silver V] Title: 뒤집기, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,18 +1,9 @@
-s = input()
+s = list(input())
 
-count0 = 0
-count1 = 0
+cnt = 0
 
-if s[0] == '1':
-  count0 += 1
-else:
-  count1 += 1
+for i in range(len(s)-1):
+  if s[i] != s[i+1]:
+    cnt += 1
 
-for i in range(len(s) - 1):
-  if s[i] != s[i + 1]:
-    if s[i + 1] == '1':
-      count0 += 1
-    else:
-      count1 += 1
-
-print(min(count0, count1))
+print((cnt + 1) // 2)
