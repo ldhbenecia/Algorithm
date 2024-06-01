a3f7a00f1823177e8a9a649f541b07327e1857db
Donghyeok Lim <jh07050@gmail.com>
2024-06-01 20:53:04 +0900

    [level 2] Title: 구명보트, Time: 7.85 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -8,18 +8,12 @@ def solution(people, limit):
     while start < end:
         if people[end] + people[start] <= limit:
             start += 1
-            end -= 1
-            answer += 1
-        else:
-            end -= 1
-            answer += 1
+            
+        end -= 1
+        answer += 1
     
     # 마지막 한 사람이 남았다면
     if start == end:
         answer += 1
         
     return answer
-
-# 정렬 후 가장 무거운 사람과 가장 가벼운 사람을 합함
-# 그 합한 값이 limit보다 작으면 함께 내리고 그렇지 않다면 무거운 사람만 내릴 것
-
