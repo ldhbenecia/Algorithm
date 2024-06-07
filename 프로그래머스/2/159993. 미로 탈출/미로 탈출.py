8422f39e6e9723d311f31411ad19a405ee64a0c7
Donghyeok Lim <jh07050@gmail.com>
2024-06-07 17:07:20 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -9,37 +9,15 @@ def solution(maps):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    def find_lever(x, y):
-        queue = deque([(x, y, 0)])
+    def bfs(start_x, start_y, end_x, end_y):
+        queue = deque([(start_x, start_y, 0)])
         visited = [[False] * b for _ in range(a)]
-        visited[x][y] = True
+        visited[start_x][start_y] = True
         
         while queue:
             x, y, time = queue.popleft()
             
-            if maps[x][y] == 'L':
-                return (x, y, time)
-            
-            for i in range(4):
-                nx = dx[i] + x
-                ny = dy[i] + y
-                
-                if 0 <= nx < a and 0 <= ny < b and not visited[nx][ny]:
-                    if maps[nx][ny] != 'X':
-                        visited[nx][ny] = True
-                        queue.append((nx, ny, time + 1))
-                        
-        return -1, -1, -1
-    
-    def escape(x, y, time):
-        queue = deque([(x, y, time)])
-        visited = [[False] * b for _ in range(a)]
-        visited[x][y] = True
-        
-        while queue:
-            x, y, time = queue.popleft()
-            
-            if maps[x][y] == 'E':
+            if x == end_x and y == end_y:
                 return time
             
             for i in range(4):
@@ -56,6 +34,16 @@ def solution(maps):
     for i in range(a):
         for j in range(b):
             if maps[i][j] == 'S':
-                lever_x, lever_y, time = find_lever(i, j)
-                return escape(lever_x, lever_y, time)
-    return -1
+                sx, sy = i, j
+            if maps[i][j] == 'L':
+                lx, ly = i, j
+            if maps[i][j] == 'E':
+                ex, ey = i, j
+            
+    find_lever = bfs(sx, sy, lx, ly)
+    escape = bfs(lx, ly, ex, ey)
+    
+    if find_lever == -1 or escape == -1:
+        return -1
+    
+    return find_lever + escape
