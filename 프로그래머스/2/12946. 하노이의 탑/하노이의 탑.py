329cab23c1606bc02fab4715280313705d734634
Donghyeok Lim <jh07050@gmail.com>
2024-06-19 20:10:14 +0900

    [level 2] Title: 하노이의 탑, Time: 11.55 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,17 +1,17 @@
-def hanoi(n, start, mid, end):
+def hanoi(n, start, end, mid):
     answer = []
     
     if n == 1:
         answer.append((start, end))
     else:
-        # Step 1: n-1 개의 원판을 start에서 end를 거쳐 mid로 옮기기
-        answer.extend(hanoi(n - 1, start, end, mid))
-        # Step 2: 남은 1개의 원판을 start에서 end로 옮기기
+        # 시작 기둥에서 보조 기둥으로 대상 기둥을 활용해서
+        answer.extend(hanoi(n - 1, start, mid, end))
+        # 시작 기둥에서 대상 기둥으로
         answer.append((start, end))
-        # Step 3: n-1 개의 원판을 mid에서 start를 거쳐 end로 옮기기
-        answer.extend(hanoi(n - 1, mid, start, end))
-    
+        # 보조 기둥에서 대상 기둥으로 시작기둥을 활용해서
+        answer.extend(hanoi(n - 1, mid, end, start))
+        
     return answer
 
 def solution(n):
-    return hanoi(n, 1, 2, 3)
+    return hanoi(n, 1, 3, 2)
