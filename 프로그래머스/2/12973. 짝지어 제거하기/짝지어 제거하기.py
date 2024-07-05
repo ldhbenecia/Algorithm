6ed1d3d54970c3d8c1a6d29db354957fd8128e5c
Donghyeok Lim <jh07050@gmail.com>
2024-07-05 16:34:55 +0900

    [level 2] Title: 짝지어 제거하기, Time: 84.38 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = 0
+    stack = []
+    
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+            
+    if not stack:
+        answer = 1
+    else:
+        answer = 0
+        
+    return answer
