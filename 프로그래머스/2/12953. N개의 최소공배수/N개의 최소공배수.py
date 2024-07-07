f565d831dc4cb161d15e3d66afeefe2307d3aad6
Donghyeok Lim <jh07050@gmail.com>
2024-07-07 21:43:44 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,13 +1,10 @@
-def get_gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+import math
 
 def solution(arr):
     n = arr[0]
     
     for i in range(1, len(arr)):
-        gcd = get_gcd(n, arr[i])
+        gcd = math.gcd(n, arr[i])
         n = n * arr[i] // gcd
     
     return n
