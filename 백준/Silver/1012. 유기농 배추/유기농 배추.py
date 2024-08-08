50ce4eba616ca4ef6a52b6ce65103c454c25021b
Donghyeok Lim <jh07050@gmail.com>
2024-08-08 15:27:25 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 114032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,35 +1,38 @@
-import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
-def dfs(x, y):
-  dx = [-1, 1, 0, 0]
-  dy = [0, 0, -1, 1]
+def bfs(x, y):
+    queue = deque([(x, y)])
     
-  for i in range(4):
-    nx = dx[i] + x
-    ny = dy[i] + y
-    
-    if (0 <= nx < n and 0 <= ny < m):
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = -1
-        dfs(nx, ny)
-          
-          
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = -1
+                    queue.append((nx, ny))
+
 t = int(input())
 
-for _ in range(t):
-  m, n, k = map(int, input().split())
-  graph = [[0] * m for _ in range(n)]
-  cnt = 0
-  
-  for _ in range(k):
-    x, y = map(int, input().split())
-    graph[y][x] = 1 # 이 문제는 행렬이 아닌 x, y 좌표계로 입력함
-    
-  for i in range(n):
-    for j in range(m):
-      if graph[i][j] > 0:
-        dfs(i, j)
-        cnt += 1
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-  print(cnt)
+for _ in range(t):
+    m, n, k = map(int, input().split())
+    graph = [[0] * m for _ in range(n)]
+    
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+    
+    count = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                count += 1
+    
+    print(count)
