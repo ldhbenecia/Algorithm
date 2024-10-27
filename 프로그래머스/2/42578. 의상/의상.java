6ccf6d11fb9aa84df3d300491912aaea40164936
Donghyeok Lim <jh07050@gmail.com>
2024-10-27 18:00:43 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,22 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> clothMap = new HashMap<>();
+        Map<String, Integer> clothMap = new HashMap<>();
 
         for (int i = 0; i < clothes.length; i++) {
-            String name = clothes[i][0];
             String type = clothes[i][1];
-            
-            if (!clothMap.containsKey(type)) {
-                clothMap.put(type, new ArrayList<>());
-            }
-            clothMap.get(type).add(name);
+            clothMap.put(type, clothMap.getOrDefault(type, 0) + 1);
         }
         
-        
         int answer = 1; // default
-        for (List<String> c : clothMap.values()) {
-            answer *= (c.size() + 1);
+        for (String key : clothMap.keySet()) {
+            answer *= (clothMap.get(key) + 1);
         }
         
         return answer - 1;
