9a906fee3a72eb273759cb0af3fb1ed6e85ba685
Donghyeok Lim <jh07050@gmail.com>
2025-03-03 22:16:17 +0900

    [Gold IV] Title: 연구소, Time: 2212 ms, Memory: 123480 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,70 +1,64 @@
 from collections import deque
 import copy
 
-# 벽 만들기
-def make_wall(count):
-  if count == 3: # 벽 3개 다 지었으면 안전영역 크기 재기
-    bfs()
-    return
-  
-  for i in range(n):
-    for j in range(m):
-      if graph[i][j] == 0:
-        graph[i][j] = 1
-        make_wall(count + 1)
-        graph[i][j] = 0
-        
-  
-# 안전영역 크기 재기
+# 풀이
+# 1. 완전 탐색(백트래킹)을 사용하여 빈 칸에 벽을 3개까지 놓는다.
+# 2. 벽을 다 놓았으면 바이러스를 퍼뜨려본다.
+# 3. 안전 영역의 개수를 센다.
+# 4. board 끝까지 이를 반복해가며 어떻게 벽을 쌓았을 때 가장 안전 영역이 큰 지 구한다.
+
+
 def bfs():
-  queue = deque()
-  test = copy.deepcopy(graph)
-  
-  for i in range(n):
-    for j in range(m):
-      if test[i][j] == 2:
-        queue.append((i, j))
-  
-  # 바이러스 전염 시작
-  while queue:
-    x, y = queue.popleft()
-    
-    for i in range(4):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      
-      if 0 <= nx < n and 0 <= ny < m:
-        if test[nx][ny] == 0:
-          test[nx][ny] = 2 # 바이러스 전염
-          queue.append((nx, ny))
-  
-  global result
-  count = 0
-  for i in range(n):
-    for j in range(m):
-      if test[i][j] == 0:
-        count += 1
-  
-  result = max(result, count)
-  
-  
+    queue = deque()
+    basic_board = copy.deepcopy(board)
+
+    # queue에 바이러스 삽입
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 2:
+                queue.append((i, j))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if basic_board[nx][ny] == 0:  # 빈 칸인가? (감염될 수 있는가)
+                    basic_board[nx][ny] = 2
+                    queue.append((nx, ny))
+
+    global answer
+    safe_area = 0
+
+    for i in range(n):
+        for j in range(m):
+            if basic_board[i][j] == 0:
+                safe_area += 1
+
+    answer = max(answer, safe_area)
+
+
+def make_wall(cnt):
+    if cnt == 3:  # 벽 다 쌓았으면 바이러스 퍼뜨려보기
+        bfs()
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 0:
+                board[i][j] = 1  # 벽 쌓기
+                make_wall(cnt + 1)  # 두 번째 벽 쌓으로 재귀
+                board[i][j] = 0  # 벽 3개 다 쌓은 후 허물기
+
+
 n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+board = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = 0
+answer = 0
 make_wall(0)
-
-print(result)
-
-
-'''
-벽을 짓는 함수, 안전영역 크기를 재는 함수(BFS) 생성
------
-빈 칸 (0인 경우)에 대해 벽을 쌓기 시작
-count == 3이 되면 bfs 탐색해서 바이러스 전염 후 안전영역 크기 카운팅
-
-안전 영역 크기 중 가장 큰 값 도출
-'''
+print(answer)
