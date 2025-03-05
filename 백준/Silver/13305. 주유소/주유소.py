2458e487e4ffaaeb21ec56078d367ff2e4d88b49
Donghyeok Lim <jh07050@gmail.com>
2025-03-06 00:05:30 +0900

    [Silver III] Title: 주유소, Time: 136 ms, Memory: 136212 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,25 +1,14 @@
-city = int(input())
+n = int(input())
 distance = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
-result = 0
-current_cost = price[0]
-for i in range(city - 1):
-    if price[i] < current_cost:
-        current_cost = price[i]
-    
-    result += current_cost * distance[i]
-        
-print(result)
+total_cost = 0
+min_price = price[0]
 
-# 도시 4개
-# [2, 3, 1] 거리
-# [5, 2, 4, 1] 기름 가격
-# 제일 왼쪽에서 제일 오른쪽으로 가야한다.
+for i in range(n - 1):
+    if price[i] < min_price:
+        min_price = price[i]
 
-# 방법 1: 5 * 6 = 30
-# 방법 2: 5 * 2 + 2 * 3 + 4 * 1 = 20
-# 방법 3: 5 * 2 + 2 * 4 = 18
+    total_cost += min_price * distance[i]
 
-# 리터당 가격을 비교해서 가장 비싼 도시에서는 바로 그 다음 칸으로 갈 만큼의 기름만 충전해야함
-# 가장 싼 도시에서는 끝까지 째면 됨
+print(total_cost)
