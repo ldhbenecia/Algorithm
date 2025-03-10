44914d6bdb756ac628ad13d7650f0afe0093d7ad
Donghyeok Lim <jh07050@gmail.com>
2025-03-10 22:47:17 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 232 ms, Memory: 122724 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,39 +1,43 @@
 from collections import deque
 
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    result[nx][ny] = result[x][y] + 1
+                    queue.append((nx, ny))
+
+
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[-1] * m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(i, j):
-  queue = deque([(i, j)])
-  visited[i][j] = 0
-  
-  while queue:
-    x, y = queue.popleft()
-    
-    for i in range(4):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      
-      if (0 <= nx < n) and (0 <= ny < m) and visited[nx][ny] == -1:
-        if graph[nx][ny] == 0:
-          visited[nx][ny] = 0
-        elif graph[nx][ny] == 1:
-          visited[nx][ny] = visited[x][y] + 1
-          queue.append((nx, ny))
-          
+visited = [[False] * m for _ in range(n)]
+result = [[-1] * m for _ in range(n)]
+
 for i in range(n):
-  for j in range(m):
-    if graph[i][j] == 2 and visited[i][j] == -1:
-      bfs(i, j)
-      
+    for j in range(m):
+        if graph[i][j] == 2 and not visited[i][j]:
+            result[i][j] = 0
+            bfs(i, j)
+
 for i in range(n):
-  for j in range(m):
-    if graph[i][j] == 0:
-      print(0, end = " ")
-    else:
-      print(visited[i][j], end = " ")
-  print()
+    for j in range(m):
+        if graph[i][j] == 0:
+            result[i][j] = 0
+
+for row in result:
+    print(*row)
