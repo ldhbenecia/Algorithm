ca237fb22e7a4f1675d1492f19a0d0e49254addf
Donghyeok Lim <jh07050@gmail.com>
2025-03-10 22:24:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 172 ms, Memory: 122548 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,41 @@
 from collections import deque
 
-def bfs(start):
-  queue = deque([(start)])
-  visitedBfs[start] = True
-  
-  while queue:
-    v = queue.popleft()
-    print(v, end = ' ')
-    
-    for i in sorted(graph[v]):
-      if not visitedBfs[i]:
-        visitedBfs[i] = True
-        queue.append(i)
-        
-def dfs(start):
-  visitedDfs[start] = True
-  print(start, end = ' ')
-  
-  for i in sorted(graph[start]):
-    if not visitedDfs[i]:
-      dfs(i)
-  
+
+def dfs(v):
+    visited_dfs[v] = True
+    print(v, end=" ")
+    for i in range(n + 1):
+        if not visited_dfs[i] and graph[v][i]:
+            dfs(i)
+
+
+def bfs(v):
+    queue = deque([v])
+    visited_bfs[v] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in range(n + 1):
+            if not visited_bfs[i] and graph[v][i]:
+                visited_bfs[i] = True
+                queue.append(i)
+
+
 n, m, v = map(int, input().split())
 
-graph = [[] for _ in range(n + 1)]
-for i in range(m):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
-  
-visitedBfs = [False] * (n + 1)
-visitedDfs = [False] * (n + 1)
+# 간선이 연결되어있다.
+graph = [[False] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+# 각 경로 방문 기록 저장 리스트
+visited_dfs = [False] * (n + 1)
+visited_bfs = [False] * (n + 1)
+
 dfs(v)
 print()
 bfs(v)
