5e6def3234052262a594819c7c4c28e8cc4aed45
Donghyeok Lim <jh07050@gmail.com>
2025-03-26 21:06:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 428 ms, Memory: 177000 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,30 +1,35 @@
 from collections import deque
 
-def bfs(start):
-  queue = deque([start])
-  visited[start] = True
-  
-  while queue:
-    v = queue.popleft()
-    
-    for i in graph[v]:
-      if not visited[i]:
-        queue.append(i)
-        visited[i] = True
+
+def bfs(x):
+    queue = deque([x])
+    visited[x] = True
+
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-count = 0
 
-for i in range(m):
-  u, v = map(int, input().split())
-  graph[u].append(v)
-  graph[v].append(u)
-  
+graph = [[] for _ in range(n + 1)]
+visited = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+
+    graph[u].append(v)
+    graph[v].append(u)
+
+count = 0
 for i in range(1, n + 1):
-  if not visited[i]:
-    bfs(i)
-    count += 1
-    
+    if not visited[i]:
+        bfs(i)
+        count += 1
+
+
 print(count)
