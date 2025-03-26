ed2c0d0fd0471fe7ba0628d01483741cca6313b4
Donghyeok Lim <jh07050@gmail.com>
2025-03-26 21:08:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 428 ms, Memory: 175112 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,17 +1,14 @@
-from collections import deque
-
-
-def bfs(x):
-    queue = deque([x])
+def dfs(x):
+    stack = [x]
     visited[x] = True
 
-    while queue:
-        v = queue.popleft()
+    while stack:
+        v = stack.pop()
 
         for i in graph[v]:
             if not visited[i]:
                 visited[i] = True
-                queue.append(i)
+                stack.append(i)
 
 
 n, m = map(int, input().split())
@@ -28,7 +25,7 @@ for _ in range(m):
 count = 0
 for i in range(1, n + 1):
     if not visited[i]:
-        bfs(i)
+        dfs(i)
         count += 1
 
 
