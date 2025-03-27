87c03a693dff8b1657005b6597b3c69a359e5c0e
Donghyeok Lim <jh07050@gmail.com>
2025-03-27 22:44:39 +0900

    [Silver I] Title: 경로 찾기, Time: 124 ms, Memory: 111040 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,43 +1,11 @@
-from collections import deque
-
-def bfs(start):
-  queue = deque([start])
-  visited = [0] * n
-  
-  while queue:
-    v = queue.popleft()
-    
-    for i in graph[v]:
-      if not visited[i]:
-        queue.append(i)
-        visited[i] = 1
-
-  return visited
-  
 n = int(input())
-graph = [[] for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 for i in range(n):
-  row = list(map(int, input().split()))
-  for j in range(n):
-    if row[j] == 1:
-      graph[i].append(j)
-      
-for i in range(n):
-  print(*bfs(i))
+    for j in range(n):
+        for k in range(n):
+            if graph[j][i] and graph[i][k]:
+                graph[j][k] = 1
 
-'''
-0 1 0
-0 0 1
-1 0 0
-
-[1]
-[2]
-[0]
-
-0번째 노드가 갈 수 있는 노드는 1번
-1번째 노드가 갈 수 있는 노드는 2번
-2번째 노드가 갈 수 있는 노드는 0번
-
-0 -> 1 -> 2 -> 0
-'''
+for i in graph:
+    print(*i)
