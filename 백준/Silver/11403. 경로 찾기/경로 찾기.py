23245f0d96e8e3bc679628d255b6aec99a548a0b
Donghyeok Lim <jh07050@gmail.com>
2025-03-27 22:54:55 +0900

    [Silver I] Title: 경로 찾기, Time: 116 ms, Memory: 111040 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,11 +1,11 @@
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if graph[j][i] and graph[i][k]:
-                graph[j][k] = 1
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
 
 for i in graph:
     print(*i)
