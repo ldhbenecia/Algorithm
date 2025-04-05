dea6d52f3063f043351c5e7658eb321e7f1c46b1
Donghyeok Lim <jh07050@gmail.com>
2025-04-05 17:17:47 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,19 +1,18 @@
-from collections import defaultdict
-
 t = int(input())
 
 for _ in range(t):
     n = int(input())
+    dic = {}
 
-    dic = defaultdict(int)
     for _ in range(n):
-        clothe = input().split()
-        name, kind = clothe[0], clothe[1]
-        dic[kind] += 1
+        name, kind = input().split()
+        if kind not in dic:
+            dic[kind] = 1
+        else:
+            dic[kind] += 1
 
     result = 1
-    for i in dic.values():
-        result *= i + 1
-    
-    result -= 1
-    print(result)
+    for kind in dic:
+        result *= dic[kind] + 1 # headgear가 2개이면, 돌아가면서 입는 경우의 수 2 + 아예 안 입는 경우의 수 1
+
+    print(result - 1) # 나체 제외
