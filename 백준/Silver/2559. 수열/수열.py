1fca3c94f7242b43e603b78c25a480134d7fcd22
Donghyeok Lim <jh07050@gmail.com>
2025-04-05 16:49:38 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 121496 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,17 +1,12 @@
-import sys
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
-temperature = list(map(int, input().split()))
+lst = list(map(int, input().split()))
 
-max_value = sum(temperature[:k])
-value = max_value
+temp = sum(lst[:k])
+result = temp
 
-for i in range(k, n):
-  value += temperature[i]
-  value -= temperature[i - k]
-  
-  if value > max_value:
-    max_value = value
-  
-print(max_value)
+for i in range(n - k):
+    temp = temp - lst[i] + lst[k + i]
+    if temp > result:
+        result = temp
+
+print(result)
