05e6d961e63bbb61256fc860622052531022f89c
Donghyeok Lim <jh07050@gmail.com>
2025-04-08 22:01:51 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,21 +1,26 @@
-from collections import Counter
+name = input()
 
-name = sorted(list(input()))
-word = Counter(name)
+dic = {}
+for i in name:
+    if i not in dic:
+        dic[i] = 1
+    else:
+        dic[i] += 1
 
-odd = 0
-odd_alphabet = ''
-result = ''
-for i in word:
-    if word[i] % 2 != 0:
-        odd += 1
-        odd_alphabet += i
-    
-        if odd >= 2:
-            print("I'm Sorry Hansoo")
-            exit()
+odd_cnt = 0
+mid = ""
 
-for i in word:
-    result += i * (word[i] // 2)
-    
-print(result + odd_alphabet + result[::-1])
+for i in sorted(dic.keys()):
+    if dic[i] % 2 == 1:
+        odd_cnt += 1
+        mid = i  # 홀수 문자는 중앙 위치 지정
+
+if odd_cnt > 1:
+    print("I'm Sorry Hansoo")
+    exit()
+
+half = ""
+for i in sorted(dic.keys()):
+    half += i * (dic[i] // 2)
+
+print(half + mid + half[::-1])
