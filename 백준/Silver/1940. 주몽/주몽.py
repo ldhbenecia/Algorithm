2dc2948f29efbcb5f2531318247c9575fba39dda
Donghyeok Lim <jh07050@gmail.com>
2025-04-08 22:10:55 +0900

    [Silver IV] Title: 주몽, Time: 100 ms, Memory: 110900 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -1,21 +1,24 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 m = int(input())
-ingredients = sorted(list(map(int, input().split())))
+num = sorted(list(map(int, input().split())))
 
-count = 0
-start = 0
-end = n - 1
+# 입력 값이 굉장히 크기 때문에 최적화를 해야할 것으로 추정
+# 이진 탐색으로 풀이하려고 했으나 2개씩 고정 -> 슬라이딩 윈도우 or 투포인터
 
-while start < end:
-  if ingredients[start] + ingredients[end] == m:
-    count += 1
-    start += 1
-  elif ingredients[start] + ingredients[end] < m:
-    start += 1
-  else:
-    end -= 1
-    
-print(count)
+left = 0
+right = n - 1
+
+result = 0
+while left < right:
+    temp = num[left] + num[right]
+
+    if temp < m:
+        left += 1
+    elif temp > m:
+        right -= 1
+    else:
+        result += 1
+        left += 1
+        right -= 1
+
+print(result)
