a68f4edf7ff7666d5b7eed2f1294ca95f34138c0
Donghyeok Lim <jh07050@gmail.com>
2025-04-10 21:02:20 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 112136 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,7 +3,6 @@ from collections import deque
 
 def bfs(x, y):
     queue = deque([(x, y)])
-    visited[x][y] = True
 
     while queue:
         x, y = queue.popleft()
@@ -12,18 +11,15 @@ def bfs(x, y):
             nx = dx[i] + x
             ny = dy[i] + y
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if not visited[nx][ny] and graph[nx][ny] == 1:
-                    queue.append((nx, ny))
-                    visited[nx][ny] = True
-                    graph[nx][ny] = graph[x][y] + 1  # 이전에 건너오기 전 값 +1
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
 
     return graph[n - 1][m - 1]
 
 
 n, m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
