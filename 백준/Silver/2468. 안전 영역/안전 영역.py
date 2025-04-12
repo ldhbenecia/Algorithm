7cbaa148f71e31d7745a27a0c2ada4aeefa25565
Donghyeok Lim <jh07050@gmail.com>
2025-04-12 21:00:49 +0900

    [Silver I] Title: 안전 영역, Time: 244 ms, Memory: 116328 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,20 +1,22 @@
 from collections import deque
 
-def bfs(x, y, height):
+
+def bfs(x, y, h, visited):
     queue = deque([(x, y)])
     visited[x][y] = True
-    
+
     while queue:
         x, y = queue.popleft()
-        
+
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-            
+
             if 0 <= nx < n and 0 <= ny < n:
-                if not visited[nx][ny] and graph[nx][ny] > height:
-                    queue.append((nx, ny))
+                if not visited[nx][ny] and graph[nx][ny] > h:
                     visited[nx][ny] = True
+                    queue.append((nx, ny))
+
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -22,22 +24,19 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# graph에 있는 숫자 중 최대 값을 찾아서 0 ~ 최댓값까지 전부 돌려서 안전한 영역이 가장 많은 값을 찾기
-max_height = 0
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] > max_height:
-            max_height = graph[i][j]
-            
-result = []        
-for height in range(max_height + 1):
+max_height = max(map(max, graph))
+result = 0
+
+for h in range(max_height + 1):  # 안오는 경우를 위해 0부터 시작함
     visited = [[False] * n for _ in range(n)]
-    safe = 0
+    safe_zone = 0
+
     for i in range(n):
         for j in range(n):
-            if not visited[i][j] and graph[i][j] > height:
-                bfs(i, j, height)
-                safe += 1
-    result.append(safe)
-    
-print(max(result))
+            if not visited[i][j] and graph[i][j] > h:
+                bfs(i, j, h, visited)
+                safe_zone += 1
+
+    result = max(result, safe_zone)
+
+print(result)
