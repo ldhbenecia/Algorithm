9f011853266227b36b430e70f15065ab0b13e58e
Donghyeok Lim <jh07050@gmail.com>
2025-04-12 20:37:10 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 114380 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,41 @@
 from collections import deque
 
+
 def bfs(x, y):
     queue = deque([(x, y)])
-    
+    graph[x][y] = 0  # visited
+
     while queue:
         x, y = queue.popleft()
-        
+
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-            
-            if 0 <= nx < n and 0 <= ny < m:
+
+            if 0 <= nx < m and 0 <= ny < n:
                 if graph[nx][ny] == 1:
-                    graph[nx][ny] = -1
                     queue.append((nx, ny))
+                    graph[nx][ny] = 0  # visit
+
 
 t = int(input())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for _ in range(t):
+for i in range(t):
     m, n, k = map(int, input().split())
-    graph = [[0] * m for _ in range(n)]
-    
+    graph = [[0] * n for _ in range(m)]
+
     for _ in range(k):
-        x, y = map(int, input().split())
-        graph[y][x] = 1
-    
-    count = 0
-    for i in range(n):
-        for j in range(m):
+        a, b = map(int, input().split())
+        graph[a][b] = 1
+
+    result = 0
+    for i in range(m):
+        for j in range(n):
             if graph[i][j] == 1:
                 bfs(i, j)
-                count += 1
-    
-    print(count)
+                result += 1
+
+    print(result)
