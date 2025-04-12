7325f045502ddb1572135338d26252d47336b001
Donghyeok Lim <jh07050@gmail.com>
2025-04-12 21:21:51 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 114444 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,24 +1,26 @@
 from collections import deque
 
+
 def bfs(x, y):
-  queue = deque([(x, y)])
-  count = 0
-  
-  graph[x][y] += 1 # 방문 처리
-  
-  while queue:
-    x, y = queue.popleft()
-    count += 1
-    
-    for i in range(4):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      
-      if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
-        graph[nx][ny] += 1
-        queue.append((nx, ny))
-        
-  return count
+    queue = deque([(x, y)])
+    graph[x][y] = 1
+    cnt = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
+                    queue.append((nx, ny))
+                    cnt += 1
+
+    return cnt
+
 
 m, n, k = map(int, input().split())
 graph = [[0] * n for _ in range(m)]
@@ -27,16 +29,18 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 for _ in range(k):
-  x1, y1, x2, y2 = map(int, input().split())
-  for i in range(y1, y2):
-    for j in range(x1, x2):
-      graph[i][j] += 1
+    x1, y1, x2, y2 = map(int, input().split())
+
+    for y in range(y1, y2):  # y가 행 (row)
+        for x in range(x1, x2):  # x가 열 (col)
+            graph[y][x] = 1
 
 result = []
 for i in range(m):
-  for j in range(n):
-    if graph[i][j] == 0:
-      result.append(bfs(i, j))
+    for j in range(n):
+        if graph[i][j] == 0:
+            result.append(bfs(i, j))
 
+result.sort()
 print(len(result))
-print(*sorted(result))
+print(*result)
