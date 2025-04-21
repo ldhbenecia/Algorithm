1956bee022c708479b847c964e5d15fe72248b78
Donghyeok Lim <jh07050@gmail.com>
2025-04-21 23:28:48 +0900

    [Silver V] Title: 사과 담기 게임, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
@@ -1,19 +1,32 @@
 n, m = map(int, input().split())
 j = int(input())
 position = [int(input()) for _ in range(j)]
+position = list(
+    map(lambda x: x - 1, position)
+)  # position은 인덱스형 위치값이 아니므로 -1씩 해서 처리
 
-start = 1
-end = m
-count = 0
+move = 0
+left = 0
+right = m - 1
 
 for i in position:
-    if i > end:
-        count += (i - end)
-        start += (i - end)
-        end = i
-    elif i < start:
-        count += (start - i)
-        end -= (start - i)
-        start = i
+    # 1. 가만히 있어도 바구니에 사과가 떨어지는 경우
+    if left <= i <= right:
+        continue
 
-print(count)
+    # 2. 사과가 떨어지는 위치가 왼쪽에 있는 경우
+    elif i < left:
+        distance = left - i  # 가야할 거리
+        move += distance
+        left -= distance
+        right -= distance
+
+    # 3. 사과가 떨어지는 위치가 오른쪽인 경우
+    else:  # i > right:
+        distance = i - right  # 가야할 거리
+        move += distance
+        left += distance
+        right += distance
+
+
+print(move)
