19b12820c9fe953fa4fe7c79c9a1fecdace47ec1
Donghyeok Lim <jh07050@gmail.com>
2025-07-01 16:50:20 +0900

    [Silver V] Title: 기상캐스터, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -1,26 +1,17 @@
 h, w = map(int, input().split())
-graph = [input() for _ in range(h)]
+board = [input() for _ in range(h)]
 
-result = []
 for i in range(h):
-    temp = []
-    count = 0
+    result = []
+    time = -1
     for j in range(w):
-        if graph[i][j] == 'c':
-            temp.append(0)
-            count = 0
-            continue
-        if 0 in temp:
-            count += 1
-            temp.append(count)
-        if not temp and graph[i][j] != 'c':
-            temp.append(-1)
-            continue
-        if temp[-1] == -1 and graph[i][j] != 'c':
-            temp.append(-1)
-            continue
-    
-    result.append(temp)
-    
-for i in result:
-    print(*i)
+        if board[i][j] == "c":
+            time = 0
+            result.append(time)
+        else:
+            if time == -1:
+                result.append(-1)
+            else:
+                time += 1
+                result.append(time)
+    print(*result)
