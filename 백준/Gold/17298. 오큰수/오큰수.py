9c4f75e18e6a2c62dbf5d494f7762565b14c6a53
Donghyeok Lim <jh07050@gmail.com>
2025-07-03 18:18:22 +0900

    [Gold IV] Title: 오큰수, Time: 516 ms, Memory: 256456 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,20 +1,11 @@
-n = int(input())
-nums = list(map(int, input().split()))
-NGE = [-1] * n # -1로 초기화
-stack = [0] # 0번 인덱스
+N = int(input())
+A = list(map(int, input().split()))
 
-# 1번 인덱스부터 비교
-for i in range(1, len(nums)):
-    while stack and nums[stack[-1]] < nums[i]:
-        NGE[stack.pop()] = nums[i]
+stack = []
+result = [-1] * N
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        result[stack.pop()] = A[i]
     stack.append(i)
-    
-print(*NGE)
-# nums의 원소가 1,000,000까지 주어질 수 있으므로 N^2 불가능
 
-# 3 5 2 7
-# stack[0] 상태
-# stack이 있고 nums[stack[-1]] = nums[0] < nums[i] = 3 < 5(i는 1부터 시작)
-# NGE[stack.pop()] = stack.pop()을 하면 인덱스 0이 나오고 NGE의 인덱스 0에 5로 변경
-# stack에 1을 삽입하면서 stack[1] 상태
-# 위와 같이 반복
+print(*result)
