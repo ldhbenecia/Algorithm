2c7426f2a6860e1bc1ba47e007df6b0c79bb97ab
Donghyeok Lim <jh07050@gmail.com>
2025-07-04 16:50:22 +0900

    [Gold V] Title: 치킨 배달, Time: 140 ms, Memory: 110984 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,34 @@
 from itertools import combinations
 
-def calculator(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def to_distance(r1, c1, r2, c2):
+    return abs(r1 - r2) + abs(c1 - c2)
+
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
 home = []
-chicken = []
+store = []
+
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
             home.append((i, j))
-        elif graph[i][j] == 2:
-            chicken.append((i, j))
+        if graph[i][j] == 2:
+            store.append((i, j))
 
-result = float("inf")
-for i in combinations(chicken, m):
-    total_distance = 0
-    for j in home:
-        distance = float('inf')
-        for k in i:
-            distance = min(distance, calculator(j, k))
-        total_distance += distance
-    result = min(result, total_distance)
-print(result)
+min_total = float("inf")
+for store_position in combinations(store, m):  # m개 치킨집 선택
+    total = 0
+    for h in home:
+        min_distance = 1e9
+        for sp in store_position:
+            distance = to_distance(h[0], h[1], sp[0], sp[1])
+            min_distance = min(min_distance, distance)
+        total += min_distance
+
+    if total < min_total:
+        min_total = total
+
+print(min_total)
