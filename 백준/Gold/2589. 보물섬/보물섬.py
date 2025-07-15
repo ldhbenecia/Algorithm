13ff3a3dd54c200f253b5154ddca1f50a723af0c
Donghyeok Lim <jh07050@gmail.com>
2025-07-15 15:02:14 +0900

    [Gold V] Title: 보물섬, Time: 796 ms, Memory: 117912 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,36 +1,45 @@
 from collections import deque
 
+
 def bfs(x, y):
-    queue = deque([(x, y, 0)])
-    visited = [[False] * w for _ in range(l)]
-    visited[x][y] = True
-    max_dist = 0
+    queue = deque([(x, y)])
+    visited = [[0] * w for _ in range(h)]
+    visited[x][y] = 1
+    cnt = 0
 
     while queue:
-        x, y, cnt = queue.popleft()
-        max_dist = max(max_dist, cnt)
+        x, y = queue.popleft()
 
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
 
-            if 0 <= nx < l and 0 <= ny < w:
-                if graph[nx][ny] == 'L' and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    queue.append((nx, ny, cnt + 1))
+            if 0 <= nx < h and 0 <= ny < w:
+                if visited[nx][ny] == 0 and graph[nx][ny] == "L":
+                    queue.append((nx, ny))
+                    visited[nx][ny] = visited[x][y] + 1
+                    cnt = max(cnt, visited[nx][ny])
 
-    return max_dist
+    return cnt
 
-l, w = map(int, input().split())
-graph = [list(input()) for _ in range(l)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = []
-for i in range(l):
-    for j in range(w):
-        if graph[i][j] == 'L':
-            result.append(bfs(i, j))
+h, w = map(int, input().split())
+graph = [list(input()) for _ in range(h)]
 
-print(max(result))
+
+result = 0
+for i in range(h):
+    for j in range(w):
+        if graph[i][j] == "L":
+            result = max(result, bfs(i, j))
+
+print(result - 1)
+
+
+# 1. 보물을 육지에서 이동할 수 있는 곳 중에서 두 곳에 놔둬본다.
+# 2. 그 중에서 가장 시간이 오래 걸린 그 위치 두 곳에 보물을 묻는다.
+# 3. 거기까지 가는데 최단 거리를 구한다.
+# 4. -> 이게 그냥 제일 먼거 구하면 그게 정답이다.
