9c6d75f5db50ac726d123997f7ef9e400616307f
Donghyeok Lim <jh07050@gmail.com>
2025-07-22 15:35:38 +0900

    [Gold IV] Title: 인구 이동, Time: 928 ms, Memory: 120156 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,62 +1,51 @@
-from collections import deque
-
-def bfs(x, y):
-    queue = deque([(x, y)])
-    group = [(x, y)] # 개통 가능한 좌표 저장 리스트
-
+def dfs(x, y):
+    stack = [(x, y)]
+    union = [(x, y)]
+    total = peoples[x][y]
     visited[x][y] = True
-    total = graph[x][y] # 초기 값 (0, 0) 위치 값
 
-    while queue:
-        x, y = queue.popleft()
+    while stack:
+        x, y = stack.pop()
 
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
 
             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if l <= abs(graph[x][y] - graph[nx][ny]) <= r:
+                if l <= abs(peoples[x][y] - peoples[nx][ny]) <= r:
                     visited[nx][ny] = True
-                    queue.append((nx, ny))
-                    group.append((nx, ny))
-                    total += graph[nx][ny]
-    
-    # 인구 이동이 있었을 경우
-    if len(group) > 1:
-        renew = total // len(group)
-        for i, j in group:
-            graph[i][j] = renew
-        return True
-    
-    return False
+                    stack.append((nx, ny))
+                    union.append((nx, ny))
+                    total += peoples[nx][ny]
+
+    return total, union
+
 
 n, l, r = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-# 조건: 동서남북으로 근접한 숫자와의 차이가 L <= diff <= R이라면 개통
-# 개통을 하면 result += 1
-# 개통을 아무데도 안하면 0임
-
-# 개통할 수 있을 때 까지 개통하고 (While True)
-# 개통을 못할때 끝내고 그때 기준으로 인구이동 몇 번 했는지 체크
+peoples = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-time = 0
+result = 0
+
 while True:
-    visited = [[False] * (n) for _ in range(n)]
-    move = False
-    
+    visited = [[False] * n for _ in range(n)]
+    is_moved = False
+
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                if bfs(i, j): # 인구이동이 있었는 지 판단
-                    move = True
+                total, union = dfs(i, j)
+                # 인구 이동이 일어났으면 해당 평균 인구수로 채워주기
+                if len(union) > 1:
+                    is_moved = True
+                    new_pop = total // len(union)
+                    for x, y in union:
+                        peoples[x][y] = new_pop
 
-    if not move:
+    if not is_moved:
         break
+    result += 1
 
-    time += 1
-
-print(time)
+print(result)
