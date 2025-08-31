e5952f75c989fa15a2363d897ffb1bea0445f963
Donghyeok Lim <jh07050@gmail.com>
2025-08-31 17:28:18 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,74 +1,57 @@
 from collections import deque
 
-# L은 오른쪽, D는 오른쪽 90도 회전
-def turn(direction, c):
-  if c == 'L':
-    direction = (direction - 1) % 4
-  else:
-    direction = (direction + 1) % 4
-    
-  return direction
-
-def simulate():
-  x, y = 1, 1 # 뱀 초기 위치 값
-  graph[x][y] = 8 # 뱀이 위치하는 곳은 8로 설정
-  direction = 1 # 내가 짠 dx, dy에서 동쪽의 인덱스는 1임
-  time = 0
-  
-  queue = deque([(x, y)]) # 뱀 위치
-  while True:
-    # 초기 동쪽 세팅 (뱀은 처음에 오른쪽을 향한다)
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    
-    # 종료조건: 벽에 부딪히거나 자기 자신에게 닿으면 종료
-    if not (1 <= nx <= n and 1 <= ny <= n) or graph[nx][ny] == 8:
-      time += 1
-      break
-    
-    # 사과가 없다면
-    if graph[nx][ny] == 0:
-      # 이동
-      graph[nx][ny] = 8
-      queue.append((nx, ny))
-      # 이동 후 이동 전 위치 삭제
-      prev_x, prev_y = queue.popleft()
-      graph[prev_x][prev_y] = 0
-      
-    # 사과가 있다면
-    elif graph[nx][ny] == 1:
-      graph[nx][ny] = 8
-      queue.append((nx, ny))
-      
-    # 이동 이후 최신화
-    x, y = nx, ny
-    time += 1
-    
-    # 방향 전환
-    if time in direction_info:
-      direction = turn(direction, direction_info[time])
-      
-  return time
-    
-
 n = int(input())
 k = int(input())
-graph = [[0] * (n + 1) for _ in range(n + 1)]
-direction_info = {}
 
-# 보드에 사과 등록
+graph = [[0] * n for _ in range(n)]  # 0: 빈칸, 1: 뱀, 2: 사과
+
 for _ in range(k):
-  col, row = map(int, input().split())
-  graph[col][row] = 1
+    col, row = map(int, input().split())
+    graph[col - 1][row - 1] = 2  # apple
 
-# 방향 회전 정보 등록
 l = int(input())
+direction_change = dict()
 for _ in range(l):
-  x, c = input().split()
-  direction_info[int(x)] = c
+    x, c = input().split()
+    direction_change[int(x)] = c
 
-# 북 동 남 서
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dx = [-1, 1, 0, 0]  # 북(0) 남(1) 서(2) 동(3)
+dy = [0, 0, -1, 1]
+direction = 3
 
-print(simulate())
+snake = deque([(0, 0)])
+graph[0][0] = 1  # 뱀 시작 위치
+
+time = 0
+x, y = 0, 0  # 벰 현재 좌표
+
+while True:
+    time += 1
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    # 벽이나 자기 몸에 부딫히면 종료
+    if not (0 <= nx < n and 0 <= ny < n) or graph[nx][ny] == 1:
+        print(time)
+        break
+
+    # 사과를 먹으면 몸 늘림
+    if graph[nx][ny] == 2:
+        graph[nx][ny] = 1
+        snake.appendleft((nx, ny))
+    else:
+        # 사과가 없으면 꼬리 빼고 이동
+        graph[nx][ny] = 1
+        snake.appendleft((nx, ny))
+        tx, ty = snake.pop()  # 꼬리 빼기
+        graph[tx][ty] = 0  # 그래프 동기화
+
+    x, y = nx, ny
+
+    # 딕셔너리에 등록해놓은 시간이 되면 방향 전환
+    # 북 남 서 동
+    if time in direction_change:
+        if direction_change[time] == "L":
+            direction = [2, 3, 1, 0][direction]
+        elif direction_change[time] == "D":
+            direction = [3, 2, 0, 1][direction]
