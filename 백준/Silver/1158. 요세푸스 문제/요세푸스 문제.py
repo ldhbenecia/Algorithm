78617d40bd7a76bf79367322421c1c6d62df76c0
Donghyeok Lim <jh07050@gmail.com>
2025-09-08 16:20:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 368 ms, Memory: 244752 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,15 +1,16 @@
-n, k = map(int, input().split())
-arr = [i for i in range(1, n + 1)]
-answer = []
-num = k - 1
+from collections import deque
 
-for i in range(n):
-    if len(arr) > num:
-        answer.append(arr.pop(num))
-        num += k - 1
-    elif len(arr) <= num:
-        num = num % len(arr)
-        answer.append(arr.pop(num))
-        num += k -1
-        
-print("<", ', '.join(str(i) for i in answer), ">", sep = '')
+n, k = map(int, input().split())
+
+people = deque(range(1, n + 1))
+killed = []
+
+while people:
+
+    # k번째 사람을 제일 앞으로 뺀다.
+    # 빼기 위해서는 그만큼의 앞에 사람을 빼서 뒤로 넣으면 된다.
+    for i in range(k - 1):
+        people.append(people.popleft())
+    killed.append(people.popleft())
+
+print("<" + ", ".join(map(str, killed)) + ">")
