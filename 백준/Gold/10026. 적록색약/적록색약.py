ba4c4e856a8b282ea4b2bdf691c63ba4d579b8c5
Donghyeok Lim <jh07050@gmail.com>
2025-09-13 19:33:33 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 112068 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,50 +1,63 @@
-import sys
-sys.setrecursionlimit(10000)
+def dfs(x, y, color):
+    stack = [(x, y)]
+    visited[x][y] = True
+
+    while stack:
+        x, y = stack.pop()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if graph[nx][ny] == color:
+                    stack.append((nx, ny))
+                    visited[nx][ny] = True
+
+
+def dfs_rg(x, y, color):
+    stack = [(x, y)]
+    visited[x][y] = True
+
+    while stack:
+        x, y = stack.pop()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if color in "RG" and graph[nx][ny] in "RG":  # R, G 묶음
+                    stack.append((nx, ny))
+                    visited[nx][ny] = True
+                elif color == "B" and graph[nx][ny] == "B":
+                    stack.append((nx, ny))
+                    visited[nx][ny] = True
+
 
 n = int(input())
-graph = [list(input()) for _ in range(n)]
-visited = [[False] * (n) for _ in range(n)]
-
-original = 0
-colorWeakness = 0
+graph = [list(input().strip()) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-    
-def dfs(x, y):
-  visited[x][y] = True
-  currentColor = graph[x][y]
-  
-  for i in range(4):
-    nx = dx[i] + x
-    ny = dy[i] + y
-    
-    if (0 <= nx < n) and (0 <= ny < n):
-      if visited[nx][ny] == False:
-        if graph[nx][ny] == currentColor:
-          dfs(nx, ny)
-        
-# 적록색약이 아닐 경우 (original)
+# 일반인
+visited = [[False] * n for _ in range(n)]
+normal = 0
 for i in range(n):
-  for j in range(n):
-    if visited[i][j] == False:
-      dfs(i, j)
-      original += 1
-      
-# 적록색약일 경우 (R -> G)
+    for j in range(n):
+        if not visited[i][j]:
+            dfs(i, j, graph[i][j])
+            normal += 1
+
+# 적록색약
+visited = [[False] * n for _ in range(n)]
+unnormal = 0
 for i in range(n):
-  for j in range(n):
-    if graph[i][j] == 'R':
-      graph[i][j] = 'G'
-      
-# 새로 세야됨 -> 초기화
-visited = [[False] * (n) for _ in range(n)] 
-  
-for i in range(n):
-  for j in range(n):
-    if visited[i][j] == False:
-      dfs(i, j)
-      colorWeakness += 1
-      
-print(original, colorWeakness)
+    for j in range(n):
+        if not visited[i][j]:
+            dfs_rg(i, j, graph[i][j])
+            unnormal += 1
+
+
+print(normal, unnormal)
