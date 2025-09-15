1e794cd1dc966d0cd4b7a4108739fc44217f5c6f
Donghyeok Lim <jh07050@gmail.com>
2025-09-15 17:23:14 +0900

    [Gold V] Title: 토마토, Time: 708 ms, Memory: 220272 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,41 +1,43 @@
 from collections import deque
 
-def bfs():
-  while queue:
-    x, y, z = queue.popleft()
-    
-    for i in range(6):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      nz = dz[i] + z
-      
-      if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and graph[nx][ny][nz] == 0:
-        graph[nx][ny][nz] = graph[x][y][z] + 1
-        queue.append((nx, ny, nz))
-        
-  for i in range(h):
-    for j in range(n):
-      for k in range(m):
-        if graph[i][j][k] == 0:
-          return -1
-      
-  max_day = max(max(max(row) for row in graph[i]) for i in range(h))
-  return max_day - 1
 
-m, n, h = map(int, input().split())
-graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+def bfs(n, m, h):
+    while queue:
+        z, y, x = queue.popleft()
+
+        for d in range(6):
+            nz = z + dz[d]
+            ny = y + dy[d]
+            nx = x + dx[d]
+            if 0 <= nz < h and 0 <= ny < m and 0 <= nx < n:
+                if graph[nz][ny][nx] == 0:
+                    graph[nz][ny][nx] = graph[z][y][x] + 1
+                    queue.append((nz, ny, nx))
+
+
+n, m, h = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(m)] for _ in range(h)]
 
 dx = [-1, 1, 0, 0, 0, 0]
 dy = [0, 0, -1, 1, 0, 0]
 dz = [0, 0, 0, 0, -1, 1]
 
-queue = deque([])
-
+queue = deque()
 for i in range(h):
-  for j in range(n):
-    for k in range(m):
-      if graph[i][j][k] == 1:
-        queue.append((i, j, k))
+    for j in range(m):
+        for k in range(n):
+            if graph[i][j][k] == 1:
+                queue.append((i, j, k))
 
-# 모든 토마토가 익었을 때의 일수 출력
-print(bfs())
+bfs(n, m, h)
+
+result = 0
+for i in range(h):
+    for j in range(m):
+        for k in range(n):
+            if graph[i][j][k] == 0:
+                print(-1)
+                exit()
+            result = max(result, graph[i][j][k])
+
+print(result - 1)
