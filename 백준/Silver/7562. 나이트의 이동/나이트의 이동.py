e2e8cf07607ded9983c0d96345b8c7c85a5259f8
Donghyeok Lim <jh07050@gmail.com>
2025-09-17 15:13:32 +0900

    [Silver I] Title: 나이트의 이동, Time: 284 ms, Memory: 128284 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -2,32 +2,34 @@ from collections import deque
 
 t = int(input())
 
-dx = [-1, -2, -2, -1, 1, 2, 2, 1]
-dy = [-2, -1, 1, 2, -2, -1, 1, 2]
+# 나이트 이동 가능 방향
+dx = [-2, -2, -1, -1, 1, 1, 2, 2]
+dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
-def bfs():
-  queue = deque()
-  queue.append((cx, cy))
-  
-  while queue:
-    x, y = queue.popleft()
-    
-    if x == mx and y == my:
-      return graph[mx][my]
-    
-    for i in range(8):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      
-      if (0 <= nx < l and 0 <= ny < l):
-        if graph[nx][ny] == 0:
-          graph[nx][ny] = graph[x][y] + 1 # cnt += 1
-          queue.append((nx, ny))
-  
 for _ in range(t):
-  l = int(input()) # 체스판의 한 변의 길이, 체스판의 크기는 l * l
-  cx, cy = map(int, input().split()) # 현재 위치
-  mx, my = map(int ,input().split()) # 이동할 위치
-  graph = [[0] * l for _ in range(l)]
-  graph[cx][cy] = 0 # cnt
-  print(bfs())
+    l = int(input())
+    graph = [[0] * l for _ in range(l)]
+    visited = [[False] * l for _ in range(l)]
+
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+
+    queue = deque([(x1, y1)])
+    visited[x1][y1] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        # base
+        if x == x2 and y == y2:
+            print(graph[x][y])
+            break
+
+        for d in range(8):
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if 0 <= nx < l and 0 <= ny < l and not visited[nx][ny]:
+                visited[nx][ny] = True
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
