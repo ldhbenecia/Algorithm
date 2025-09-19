5699fe31217219bae09ba91dae3b938bbe6947bb
Donghyeok Lim <jh07050@gmail.com>
2025-09-19 16:17:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 113496 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,43 @@
+from collections import deque
+
+
+def bfs(x, y):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+    home = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for d in range(4):
+            nx = dx[d] + x
+            ny = dy[d] + y
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and graph[nx][ny] == 1:
+                    queue.append((nx, ny))
+                    visited[nx][ny] = True
+                    home += 1
+
+    return home
+
+
 n = int(input())
-
 graph = [list(map(int, input())) for _ in range(n)]
-visited = [[False] * (n) for _ in range(n)]
-
-cnt = 0 # 각 단지내 집 수
-result = []
-
+visited = [[False] * n for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y):
-  global cnt
-  visited[x][y] = True
-  cnt += 1
-  
-  for i in range(4):
-    nx = dx[i] + x
-    ny = dy[i] + y
-    
-    if (0 <= nx < n and 0 <= ny < n):
-      if visited[nx][ny] == False:
-        if graph[nx][ny] == 1:
-          visited[nx][ny] = True
-          dfs(nx, ny)
-
-  return cnt
-
-result = []
+total = 0
+group = []
 
 for i in range(n):
-  for j in range(n):
-    if graph[i][j] == 1 and visited[i][j] == False:
-      result.append(dfs(i, j))
-      cnt = 0
+    for j in range(n):
+        if graph[i][j] == 1 and not visited[i][j]:
+            home_cnt = bfs(i, j)
+            group.append(home_cnt)
+            total += 1
 
-result.sort()
-
-print(len(result))
-for i in result:
-  print(i)
+print(total)
+for i in sorted(group):
+    print(i)
