6594a097aaa614d564cc63b4981d00b66ab8606a
Donghyeok Lim <jh07050@gmail.com>
2025-09-19 16:36:09 +0900

    [Gold V] Title: 상범 빌딩, Time: 156 ms, Memory: 115928 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -1,49 +1,63 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
-def bfs(x, y, z):
-  queue = deque([(x, y, z)])
-  building[x][y][z] = 1 # 방문표시와 함께 minute 세기
-  
-  while queue:
-    x, y, z = queue.popleft()
-    
-    for i in range(6):
-      nx = dx[i] + x
-      ny = dy[i] + y
-      nz = dz[i] + z
-      
-      if 0 <= nx < L and 0 <= ny < R and 0 <= nz < C:
-        if building[nx][ny][nz] == '.':
-          building[nx][ny][nz] = building[x][y][z] + 1
-          queue.append((nx, ny, nz))
-        elif building[nx][ny][nz] == 'E':
-          return building[x][y][z]
-          
-  return 'Trapped!'
+input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0, 0, 0]
 dy = [0, 0, -1, 1, 0, 0]
 dz = [0, 0, 0, 0, -1, 1]
 
+
+def bfs(start, end):
+    queue = deque()
+    visited = [[[False] * c for _ in range(r)] for _ in range(l)]
+    sz, sy, sx = start
+    queue.append((sz, sy, sx, 0))
+    visited[sz][sy][sx] = True
+
+    while queue:
+        z, y, x, dist = queue.popleft()
+        if (z, y, x) == end:
+            return dist
+
+        for d in range(6):
+            nz = dz[d] + z
+            ny = dy[d] + y
+            nx = dx[d] + x
+
+            if 0 <= nz < l and 0 <= ny < r and 0 <= nx < c:
+                if not visited[nz][ny][nx] and graph[nz][ny][nx] != "#":
+                    queue.append((nz, ny, nx, dist + 1))
+                    visited[nz][ny][nx] = True
+
+    return -1
+
+
 while True:
-  L, R, C = map(int, input().split())
-  
-  if L == 0 and R == 0 and C == 0:
-    break
-  
-  building = []
-  for _ in range(L):
-    building.append([list(input().strip()) for _ in range(R)])
-    input()
-    
-  for i in range(L):
-    for j in range(R):
-      for k in range(C):
-        if building[i][j][k] == 'S':
-          result = bfs(i, j, k)
-          if result == 'Trapped!':
-            print("Trapped!")
-          else:
-            print(f"Escaped in {result} minute(s).")
+    l, r, c = map(int, input().split())
+
+    # base
+    if l == 0 and r == 0 and c == 0:
+        break
+
+    start = end = None
+    graph = []
+
+    for z in range(l):
+        floor = []
+        for y in range(r):
+            row = list(input().strip())
+            for x in range(c):
+                if row[x] == "S":
+                    start = (z, y, x)
+                elif row[x] == "E":
+                    end = (z, y, x)
+            floor.append(row)
+        graph.append(floor)
+        input()
+
+    result = bfs(start, end)
+    if result == -1:
+        print("Trapped!")
+    else:
+        print(f"Escaped in {result} minute(s).")
