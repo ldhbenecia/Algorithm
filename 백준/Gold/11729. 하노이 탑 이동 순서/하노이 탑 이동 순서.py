599202068bfa5561aa5e42e4d009708eb1866b00
Donghyeok Lim <jh07050@gmail.com>
2025-09-21 18:59:19 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 448 ms, Memory: 218236 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,15 +1,29 @@
-def hanoi(n, start, mid, end):
+def hanoi(n, start, end, temp):
+    global cnt, path
     # base
     if n == 1:
-        print(start, end)
+        cnt += 1
+        path.append((start, end))
         return
 
     # start: 옮길 원판의 위치, end: 원판 도착 위치
-    hanoi(n - 1, start, end, mid) # 가운데에 n-1개를 이동 시킴
-    print(start, end)
-    hanoi(n - 1, mid, start, end) # n-1개를 모두 마지막에 위치
+    hanoi(n - 1, start, temp, end)
+    cnt += 1
+    path.append((start, end))
+    hanoi(n - 1, temp, end, start)  # n-1개를 모두 마지막에 위치,
 
+
+# 원판이 3개라고 가정할 시 제일 아래에 있는 원판을 end 지점으로 옮기기 위해 그 위의 n-1개의 원판을 mid에 옮긴다.
+# 시작점: start, 도착지점: temp, temp: end
+# 그 후 temp에 있는 원판을 end로 옮긴다. 시작점: temp, 도착지점: end, temp: start
+# 가장 마지막 원판은 출력하면 끝
 
 k = int(input())
-print(2**k - 1)
-hanoi(k, 1, 2, 3)
+
+cnt = 0
+path = []
+
+hanoi(k, 1, 3, 2)
+print(cnt)
+for i, j in path:
+    print(i, j)
