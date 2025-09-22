3908ebb56c0e2cd818e838ea0bb247e4189cbcd5
Donghyeok Lim <jh07050@gmail.com>
2025-09-22 19:54:44 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 111492 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,22 @@
-n, m = map(int, input().split())
+import sys
 
-result = []
+input = sys.stdin.readline
+
 
 def dfs():
-  if len(result) == m:
-    print(" ".join(map(str, result)))
-    return
-  
-  for i in range(1, n + 1):
-    if i not in result:
-      result.append(i)
-      dfs()
-      result.pop()
-    
+    # base
+    if len(result) == m:
+        print(" ".join(map(str, result)))
+        return
+
+    for i in range(1, n + 1):
+        if i not in result:
+            result.append(i)
+            dfs()
+            result.pop()
+
+
+n, m = map(int, input().split())
+result = []
+
 dfs()
