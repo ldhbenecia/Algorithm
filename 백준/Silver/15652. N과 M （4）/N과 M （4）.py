a2a22883c3a84afd25ff793ff79ac89bd3d02a98
Donghyeok Lim <jh07050@gmail.com>
2025-09-24 12:42:28 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 112092 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,14 +1,15 @@
+def backtracking(depth, start):
+    # base
+    if depth == m:
+        print(*temp)
+        return
+
+    for i in range(start, n + 1):
+        temp.append(i)
+        backtracking(depth + 1, i)
+        temp.pop()
+
+
 n, m = map(int, input().split())
-
-result = []
-def backtracking(start):
-  if len(result) == m:
-    print(*result)
-    return 
-
-  for i in range(start, n + 1):
-    result.append(i)
-    backtracking(i)
-    result.pop()
-    
-backtracking(1)
+temp = []
+backtracking(0, 1)
