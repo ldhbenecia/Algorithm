29cc8ab0426961a3fe25dac0cdebec7c6587e678
Donghyeok Lim <jh07050@gmail.com>
2025-09-24 12:30:12 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,16 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def backtracking(depth):
+    # base
+    if depth == m:
+        print(*temp)
+        return
+
+    for i in range(1, n + 1):
+        if not visited[i]:
+            if temp and i + 1 <= temp[-1]:
+                continue
+            visited[i] = True
+            temp.append(i)
+            backtracking(depth + 1)
+            temp.pop()
+            visited[i] = False
+
+
 n, m = map(int, input().split())
-
-result = []
-
-def dfs(start):
-  if len(result) == m:
-    print(*result)
-    return
-  
-  for i in range(start, n + 1):
-    if i not in result:
-      result.append(i)
-      dfs(i + 1)
-      result.pop()
-    
-dfs(1)
+temp = []
+visited = [False] * (n + 1)
+backtracking(0)
+# 완전탐색 -> 중복되는 수는 걸러줘야함 -> 백트래킹
