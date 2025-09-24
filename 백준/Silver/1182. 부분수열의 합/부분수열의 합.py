b6c7aa44c1e164f4330d1489ba208663c1021329
Donghyeok Lim <jh07050@gmail.com>
2025-09-24 12:12:23 +0900

    [Silver II] Title: 부분수열의 합, Time: 204 ms, Memory: 111672 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,15 +1,31 @@
 import sys
-from itertools import combinations
 
 input = sys.stdin.readline
 
+
+def backtracking(idx, sum):
+    global result
+
+    # base
+    if idx == n:
+        if sum == s:
+            result += 1
+        return
+
+    # 현재 원소 포함
+    backtracking(idx + 1, sum + nums[idx])
+    # 현재 원소 미포함
+    backtracking(idx + 1, sum)
+
+
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
-count = 0
 
-for r in range(1, n + 1):
-  for comb in combinations(nums, r):
-    if sum(comb) == s:
-      count += 1
+result = 0
+backtracking(0, 0)
 
-print(count)
+# 공집합(아무것도 선택하지 않은 경우)은 제외해야 함
+if s == 0:
+    result -= 1
+
+print(result)
