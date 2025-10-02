4802d8f9d69e19d08b8996d6f81e3d349893fdaf
Donghyeok Lim <jh07050@gmail.com>
2025-10-02 21:35:06 +0900

    [Silver II] Title: N과 M (11), Time: 336 ms, Memory: 124720 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,20 +1,27 @@
-def backtracking(length):
-  # base 조건: m만큼 수열을 생성하면 출력
-  if length == m:
-    print(*result)
-    return
-  
-  for i in range(n):
-    # 이전 원소와 현재 원소가 같은 경우 건너뛰기
-    if i > 0 and nums[i - 1] == nums[i]: 
-      continue
-    result.append(nums[i])
-    backtracking(length + 1)
-    result.pop()
-  
+import sys
+
+input = sys.stdin.readline
+
+
+def backtracking(depth):
+    # base
+    if depth == m:
+        print(*temp)
+        return
+
+    prev = None
+    for i in range(n):
+        if prev == nums[i]:
+            continue
+
+        prev = nums[i]
+        temp.append(nums[i])
+        backtracking(depth + 1)
+        temp.pop()
+
+
 n, m = map(int, input().split())
 nums = sorted(list(map(int, input().split())))
 
-result = []
-
+temp = []
 backtracking(0)
