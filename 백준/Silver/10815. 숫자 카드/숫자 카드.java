b8f85bb83b1a13ab0bbde12694d24ce0f1457a38
Donghyeok Lim <jh07050@gmail.com>
2025-11-03 16:15:37 +0900

    [Silver V] Title: 숫자 카드, Time: 3188 ms, Memory: 131920 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] cards = new int[N];
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cards);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] targets = new int[M];
+        for (int i = 0; i < M; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
+
+            bw.write(binarySearch(targets[i], cards) ? "1 " : "0 ");
+            bw.flush();
+        }
+    
+    }
+
+    private static boolean binarySearch(int target, int[] cards) {
+        int left = 0;
+        int right = cards.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int val = cards[mid];
+
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}   
