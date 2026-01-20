99decae6fc8a30276fbbf8dad28e1b6ded325360
Donghyeok Lim <jh07050@gmail.com>
2026-01-20 14:43:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.13 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        Map<String, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
+        // 동명이인이 있을 수 있기 때문에 이름과 몇 명인지 연산
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        // 완주한 사람 빼기
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        
+        // map에 0이 아닌 사람 추출하면 답
+        // Entry를 사용할 때는 getKey(), getValue()만 사용
+        // Map에서 꺼낼 때는 ketSet(), values() 사용
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                return entry.getKey();
             }
         }
         
-        return participant[participant.length - 1];
+        return "";
     }
 }
