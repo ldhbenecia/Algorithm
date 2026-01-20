1f8c768d2fce18d949fcfb7663821e050cd53152
Donghyeok Lim <jh07050@gmail.com>
2026-01-20 14:52:33 +0900

    [level 1] Title: 폰켓몬, Time: 12.27 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,18 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int target = nums.length / 2;
         
-        Set<Integer> uniqueNumbers = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            uniqueNumbers.add(nums[i]);
+        
+        // 종류 번호, 마릿수 저장
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
- 
-        if (uniqueNumbers.size() >= target) {
-            answer = target;
-        } else {
-            answer = uniqueNumbers.size();
+        
+        int possible = nums.length / 2;
+        // possible 개수만큼 가져가도 됨
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (possible == 0) break;
+            
+            if (entry.getValue() > 0) {
+                answer++;
+                possible--;
+            }
         }
+        
+        
         
         return answer;
     }
