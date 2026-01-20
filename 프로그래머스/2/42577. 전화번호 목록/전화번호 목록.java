233d02f33e07c4d74f0f236a27ee7fa41519f46d
Donghyeok Lim <jh07050@gmail.com>
2026-01-20 15:11:07 +0900

    [level 2] Title: 전화번호 목록, Time: 305.90 ms, Memory: 246 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {        
-        Arrays.sort(phone_book);
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
         
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        for (String phone : phone_book) {
+            set.add(phone);
+        }
+        
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+                
+                if (set.contains(prefix)) return false;
             }
         }
-    
+        
         return true;
     }
 }
